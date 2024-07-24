a68f404b99c59d3a363441849d413a8c738cc67b
jijin <jijin@Sneha-jijin>
2024-07-24 12:59:02 +0530

    polymorphism

---
+++ SamplePgm/src/main/java/polymorphism/OffSeason.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class OffSeason {
+	
+	
+	public void discount(int amnt) {
+	int pr = (15*amnt)/100;	
+	System.out.println("Discount amnt is:"+pr);	
+	}
+
+}
---
+++ SamplePgm/src/main/java/polymorphism/OnSeason.java
@@ -0,0 +1,20 @@
+package polymorphism;
+
+public class OnSeason extends OffSeason {
+	
+
+	public void discount(int amnt) {
+		int price = (40*amnt)/100;
+		System.out.println("Discount amnt is:"+price);
+		
+	}
+
+	public static void main(String[] args) {
+		OnSeason obj = new OnSeason();
+		obj.discount(2000);
+		OffSeason of = new OffSeason();
+		of.discount(1000);
+
+	}
+
+}
