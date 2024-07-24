30841847171ab853f54aedfed575bebdd83978f6
jijin <jijin@Sneha-jijin>
2024-07-24 15:54:40 +0530

    polymorphism

--- SamplePgm/src/main/java/polymorphism/OnSeason.java
+++ SamplePgm/src/main/java/polymorphism/OnSeason.java
@@ -5,7 +5,7 @@ public class OnSeason extends OffSeason {
 
 	public void discount(int amnt) {
 		int price = (40*amnt)/100;
-		System.out.println("Printing Data is");
+		System.out.println("Printing Data are");
 		System.out.println("Discount amnt is:"+price);
 		
 	}
